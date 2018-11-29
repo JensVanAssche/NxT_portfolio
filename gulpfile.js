e1b7fad194972dc8213be201be25627ba7063595
JensVanAssche <jens.va@hotmail.com>
2018-11-29 20:25:27 +0100

    project startup

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+ 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+// const imagemin = require('gulp-imagemin');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', () => 
+  gulp.src('./src/scss/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'))
+);
+
+// gulp.task('imagemin', () =>
+//   gulp.src('src/images/*')
+//     .pipe(imagemin())
+//     .pipe(gulp.dest('dist/images'))
+// );
+ 
+gulp.task('sass:watch', () =>
+  gulp.watch('./sass/**/*.scss', ['sass'])
+);
+
+gulp.task('default', ['sass:watch']);
