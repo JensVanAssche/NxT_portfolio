8c7d40591647c879dcde95b49b73e984a06f9fae
JensVanAssche <jens.va@hotmail.com>
2019-01-29 14:45:22 +0100

    scroll nav mobile

--- src/js/nav_scroll.js
+++ src/js/nav_scroll.js
@@ -7,51 +7,53 @@ var scaleThreshold = startLeft - (leftInterval * 8);
 
 // on scroll
 $(window).on('mousewheel', function(e){
-	var delta = e.originalEvent.wheelDelta;
-	var navRight = ($(window).width() - ($("nav").offset().left + $("nav").outerWidth()));
-	
-    // scroll down
-    if (delta < 0) {
-    	// if the scale is not the minimum
-    	if (scale > 0.1) {
-			scale -= scaleInterval;
-    		$("#hoverlock").addClass("active");
-    	}
+	if ($(window).width() > 1200) {
+		var delta = e.originalEvent.wheelDelta;
+		var navRight = ($(window).width() - ($("nav").offset().left + $("nav").outerWidth()));
+		
+		// scroll down
+		if (delta < 0) {
+			// if the scale is not the minimum
+			if (scale > 0.1) {
+				scale -= scaleInterval;
+				$("#hoverlock").addClass("active");
+			}
 
-    	if (left <= $(window).width() / 2) {
-    		// hide the scroll icon
-    		$(".scroll-downs").addClass("hidden");
-    	}
+			if (left <= $(window).width() / 2) {
+				// hide the scroll icon
+				$(".scroll-downs").addClass("hidden");
+			}
 
-		if (navRight < 0) {
-			left -= leftInterval;
+			if (navRight < 0) {
+				left -= leftInterval;
+			}
 		}
-    }
-    // scroll up
-    else if (delta > 0) {
-    	// if the nav is past the middle point
-    	if (left >= scaleThreshold) {
-    		// if the scale is not the maximum
-    		if (scale < 1) {
-	    		scale += scaleInterval;
-	    	}
-	    	else {
-	    		$("#hoverlock").removeClass("active");
-	    	}
-    	}
-        
-        // dont scroll further if the nav is offscreen
-        if (left < startLeft) {
-    		left += leftInterval;
-    	}
-    }
+		// scroll up
+		else if (delta > 0) {
+			// if the nav is past the middle point
+			if (left >= scaleThreshold) {
+				// if the scale is not the maximum
+				if (scale < 1) {
+					scale += scaleInterval;
+				}
+				else {
+					$("#hoverlock").removeClass("active");
+				}
+			}
+			
+			// dont scroll further if the nav is offscreen
+			if (left < startLeft) {
+				left += leftInterval;
+			}
+		}
 
 
-	// set the main logo style
-	$("#mainlogo").css({'transform': 'scale(' + scale + ')', 'opacity': scale});
+		// set the main logo style
+		$("#mainlogo").css({'transform': 'scale(' + scale + ')', 'opacity': scale});
 
-    // set the nav style
-	$("nav").css({'left': left + 'px'});
+		// set the nav style
+		$("nav").css({'left': left + 'px'});
+	}
 });
 
 $(window).on('resize', function(e){
